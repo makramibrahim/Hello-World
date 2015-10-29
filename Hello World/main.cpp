b15cd09c83c8006f70f3d664a851714ea4eaa43d
Makram Ibrahim <makramibrahim@makrams-macbook-pro.local>
2015-10-28 23:54:17 -0600

    Initial Commit

---
+++ Hello World/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Hello World
+//
+//  Created by Makram Ibrahim on 10/28/15.
+//  Copyright © 2015 Makram Ibrahim. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
